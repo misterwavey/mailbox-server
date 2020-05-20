1e3c2222074866131d29f03eb1a7e17bfaf14efc
misterwavey <github@wavey.org>
2020-05-20 22:38:38 +0100

    first test refactor

--- mailbox-server/test_server_unittest.py
+++ mailbox-server/test_server_unittest.py
@@ -2,18 +2,22 @@ import unittest
 import socket
 
 class TestServer(unittest.TestCase):
-	INVALID_REQUEST = bytes([0,2])
-	EXPECTED = bytes([4])
+	INVALID_REQUEST = bytes([0,1])
+	EXPECTED = bytes([5])
 
 	def setUp(self):
 		self.client = socket.socket()
 		self.client.settimeout(1)
 		self.client.connect(('127.0.0.1', 8080))
 
-	def testInvalidLength(self):
-		self.client.send(self.INVALID_REQUEST)
+	def get_response_for_request(self, request):
+		self.client.send(request)
 		response = self.client.recv(1024)
 		self.client.close()
+		return response
+
+	def testInvalidLength(self):
+		response = self.get_response_for_request(self.INVALID_REQUEST)
 		self.assertEqual(response, self.EXPECTED, "not expected")
 
 if __name__ == '__main__':
