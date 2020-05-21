dfa8f608c60f0ca4b57f1dcb71ba8e4cda11f12f
misterwavey <github@wavey.org>
2020-05-21 09:45:09 +0100

    test fixtures setup

--- mailbox-server/test_server_unittest.py
+++ mailbox-server/test_server_unittest.py
@@ -2,23 +2,52 @@ import unittest
 import socket
 
 class TestServer(unittest.TestCase):
-	INVALID_REQUEST = bytes([0,1])
-	EXPECTED = bytes([5])
+	
+	test_cases = [
+			[
+	       		"request too short",
+	       		bytes([0, 1, 1, 1]),
+	       		bytes([5])
+	 		],
+	    	[
+	    		"unsupported protocol",	
+	    		bytes([0, 0, 1, 1, 98, 97, 104, 111, 106, 115, 105, 98, 111, 102, 108, 111, 98, 117, 116, 115, 117, 106, 97, 114]),
+	    		bytes([1])
+	       ],
+	       [
+	       		"invalid cmd",
+	       		bytes([0, 1, 10, 1, 98, 97, 104, 111, 106, 115, 105, 98, 111, 102, 108, 111, 98, 117, 116, 115, 117, 106, 97, 114]),
+	       		bytes([2])
+	       ],
+	       [
+	       		"invalid userid",
+	       		bytes([0, 1, 1, 1, 32, 97, 104, 111, 106, 115, 105, 98, 111, 102, 108, 111, 98, 117, 116, 115, 117, 106, 97, 114]),
+	       		bytes([103])
+	       ],
+	       [
+	       		"register bahojsiboflobutsujar",
+	       		bytes([0, 1, 1, 1, 98, 97, 104, 111, 106, 115, 105, 98, 111, 102, 108, 111, 98, 117, 116, 115, 117, 106, 97, 114]),
+	       		bytes([201, 115, 116, 117, 97, 114, 116, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+	       ]
+	    ]
 
 	def setUp(self):
 		self.client = socket.socket()
 		self.client.settimeout(1)
 		self.client.connect(('127.0.0.1', 8080))
 
+	def tearDown(self):
+		self.client.close()
+
 	def get_response_for_request(self, request):
 		self.client.send(request)
 		response = self.client.recv(1024)
-		self.client.close()
 		return response
 
-	def testInvalidLength(self):
-		response = self.get_response_for_request(self.INVALID_REQUEST)
-		self.assertEqual(response, self.EXPECTED, "not expected")
+	def testIt(self):
+		for name,request,expected_response in self.test_cases:
+			response = self.get_response_for_request(request)
+			self.assertEqual(response, expected_response, "test " + name + " failed. response: " + str(list(response)) + " not: " + str(list(expected_response)))
 
 if __name__ == '__main__':
     unittest.main()		
